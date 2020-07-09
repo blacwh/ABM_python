c5852a0cdb3bcbd48e640381daf6b5eba42aa4e7
blacwh <blacwh@gmail.com>
2020-07-09 22:15:30 +0900

    added migrate model

---
+++ test.py
@@ -0,0 +1,10 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+for i in range(20):
+    it = np.random.uniform(0.05,0.95)
+    ia = np.random.random() * 10
+    # vr = 0.1 * np.sqrt(np.random.random()) + 0.05
+    # vphi = 2*np.pi * np.random.random()
+    # vx, vy = vr * np.cos(vphi), vr * np.sin(vphi) * 0.5  
+    print(it)
